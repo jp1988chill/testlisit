a10966f54a24a9c8f3aab557584c22779bf888dd
Juan Pablo <juan.pablo.nv2015@hotmail.com>
2021-12-07 12:00:30 -0300

    add EF Core connection and usability Unit Test in RestAPISwagger

--- RestAPISwagger/Prueba.UnitTests/PruebaUnitTest.cs
+++ RestAPISwagger/Prueba.UnitTests/PruebaUnitTest.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
 using Prueba.Domain;
 using Prueba.Repository;
+using Prueba.WebApi.Controllers;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -22,18 +26,54 @@ namespace Prueba.UnitTests
     [TestClass]
     public class PruebaUnitTest
     {
-        /////////////////////////Métodos ASP .NET Core 3.x: Implementación Rest API Microservicios Inicio /////////////////////////
-       
+        private readonly IConfiguration Configuration;
+        private readonly string _sqlCon;
         public PruebaUnitTest()
         {
-            
+            var services = new ServiceCollection().AddEntityFrameworkSqlServer();
+
+            //Add IOption dependency
+            services.AddOptions();
+
+            //Build a new Configuration required by Client Rest API Controller
+            Configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            _sqlCon = Configuration.GetConnectionString("database");
+        }
+
+        private async Task<DatabaseContext> GetDatabaseContext()
+        {
+            var options = new DbContextOptionsBuilder<DatabaseContext>().UseSqlServer(_sqlCon)
+                .Options;
+            var databaseContext = new DatabaseContext(options);
+            if (!databaseContext.Database.IsInMemory())
+            {
+                databaseContext.Database.Migrate();
+            }
+            databaseContext.Database.EnsureCreated();
+            for (int i = 1; i <= 10; i++)
+            {
+                databaseContext.Users.Add(new User(name: "TestName", password: "TestPassword", token: new Guid(), tokenleasetime: ""));
+                databaseContext.SaveChanges();
+            }
+            return databaseContext;
         }
 
         [TestMethod]
-        public void TestMethod1()
+        public void TestEFCoreConnection()
         {
-            //Implementar llamadas Rest aquí (mismas que Cliente WebAPI) además de lógica de negocio
-            
+            //Testea conexión a EF Core
+            //Arrange
+            var con = GetDatabaseContext().Result;
+            con.Database.OpenConnection();
+            var dbcon = con.Database.GetDbConnection();
+            Assert.AreEqual(dbcon.State, ConnectionState.Open);
+            con.Database.ExecuteSqlRaw("TRUNCATE TABLE [Users]");
+            con.Database.CloseConnection();
+            Assert.AreEqual(dbcon.State, ConnectionState.Closed);
         }
     }
 }
---
+++ RestAPISwagger/Prueba.UnitTests/UnitTestDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Prueba.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prueba.UnitTests
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected DatabaseContext()
+        {
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
